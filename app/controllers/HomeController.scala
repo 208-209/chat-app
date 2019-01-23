82318c22ea9354a52a768b61b656ee1e59387552
208-209 <208_209@outlook.jp>
2019-01-23 17:32:33 +0900

    トップページにログイン情報が表示されるよう修正

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -11,6 +11,12 @@ import models._
 class HomeController @Inject()(val cache: SyncCacheApi, cc: ControllerComponents) extends TwitterLoginController(cc) with I18nSupport {
 
   def index() = TwitterLoginAction { implicit request: TwitterLoginRequest[AnyContent] =>
-    Ok(views.html.index(None))
+    request.accessToken match {
+      case Some(token) =>
+        val user = userFindById(token.getUserId)
+        Ok(views.html.index(user))
+      case None =>
+        Ok(views.html.index(None))
+    }
   }
 }
