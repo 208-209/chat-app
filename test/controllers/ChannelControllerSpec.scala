64fc511533f7aebea0974998e9bd35cb50ab93ca
208-209 <208_209@outlook.jp>
2019-01-30 20:43:50 +0900

    ChannelControllerのテストを一部実装

---
+++ test/controllers/ChannelControllerSpec.scala
@@ -0,0 +1,19 @@
+package controllers
+
+import org.scalatestplus.play._
+import org.scalatestplus.play.guice._
+import play.api.test._
+import play.api.test.Helpers._
+
+class ChannelControllerSpec extends PlaySpec with GuiceOneAppPerTest with Injecting {
+
+  "GET /general" should {
+
+    "Twitterの認証なしにチャンネルにアクセスすると、Twitter認証のページにリダイレクトされる" in {
+      val controller = inject[ChannelController]
+      val home = controller.read("general").apply(FakeRequest(GET, "/general"))
+
+      status(home) mustBe 303
+    }
+  }
+}
