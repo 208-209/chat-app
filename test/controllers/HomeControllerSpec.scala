ebcdf921b8a9a7b838d6742cde1b8f7a1dfaf3b6
208-209 <208_209@outlook.jp>
2019-01-30 19:49:29 +0900

    HomeControllerのテストを実装

---
+++ test/controllers/HomeControllerSpec.scala
@@ -0,0 +1,31 @@
+package controllers
+
+import org.scalatestplus.play._
+import org.scalatestplus.play.guice._
+import play.api.test._
+import play.api.test.Helpers._
+
+class HomeControllerSpec extends PlaySpec with GuiceOneAppPerTest with Injecting {
+
+  "GET /" should {
+
+    "タイトルが表示される" in {
+      val controller = inject[HomeController]
+      val home = controller.index().apply(FakeRequest(GET, "/"))
+
+      status(home) mustBe OK
+      contentType(home) mustBe Some("text/html")
+      contentAsString(home) must include ("Chat app")
+    }
+
+
+    "ログインのためのリンクが含まれる" in {
+      val controller = inject[HomeController]
+      val home = controller.index().apply(FakeRequest(GET, "/"))
+
+      status(home) mustBe OK
+      contentType(home) mustBe Some("text/html")
+      contentAsString(home) must include ("""<a href="/login"""")
+    }
+  }
+}
